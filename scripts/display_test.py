c24a3b7fa8e4a8928167c8c57ec0b8bfa9d704b8
meghals4218 <meghals4218@gmail.com>
2026-05-31 20:53:02 +0100

    added test script to display

--- scripts/display_test.py
+++ scripts/display_test.py
@@ -6,33 +6,29 @@ from adafruit_rgb_display import st7789
 
 spi = board.SPI()
 
-cs_pin = digitalio.DigitalInOut(board.CE0)    # GPIO8
-dc_pin = digitalio.DigitalInOut(board.D25)    # GPIO25
-reset_pin = digitalio.DigitalInOut(board.D24) # GPIO24
+cs_pin = digitalio.DigitalInOut(board.CE0)     # GPIO8
+dc_pin = digitalio.DigitalInOut(board.D25)     # GPIO25
+reset_pin = digitalio.DigitalInOut(board.D24)  # GPIO24
 
-rotations = [0, 90, 180, 270]
+disp = st7789.ST7789(
+    spi,
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    width=320,
+    height=240,
+    rotation=0,
+    baudrate=8000000,
+)
 
-for rotation in rotations:
-    print(f"Trying rotation {rotation}")
+image = Image.new("RGB", (320, 240), "black")
+draw = ImageDraw.Draw(image)
 
-    disp = st7789.ST7789(
-        spi,
-        cs=cs_pin,
-        dc=dc_pin,
-        rst=reset_pin,
-        width=320,
-        height=240,
-        rotation=rotation,
-        baudrate=24000000,
-    )
+draw.rectangle((0, 0, 319, 239), outline="white")
+draw.text((20, 30), "TFT TEST", fill="white")
+draw.text((20, 70), "Rotation 0", fill="green")
+draw.text((20, 110), "320 x 240", fill="yellow")
 
-    image = Image.new("RGB", (320, 240), "black")
-    draw = ImageDraw.Draw(image)
+disp.image(image)
 
-    draw.rectangle((0, 0, 319, 239), outline="white")
-    draw.text((20, 30), f"ROTATION {rotation}", fill="white")
-    draw.text((20, 70), "TFT TEST", fill="green")
-    draw.text((20, 110), "If readable, it works", fill="yellow")
-
-    disp.image(image)
-    time.sleep(5)
+time.sleep(30)
